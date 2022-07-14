1cc9e77722dcf7e1383152b3b53f549580a59a4f
sudobooo <marsdoghelena@gmail.com>
2022-07-14 16:08:56 +0300

    Fix

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -14,8 +14,9 @@ from pathlib import Path
 from dotenv import load_dotenv
 import os
 
+
 load_dotenv()
-env_path = Path('..')/'.env'
+env_path = Path('..') / '.env'
 load_dotenv(dotenv_path=env_path)
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
