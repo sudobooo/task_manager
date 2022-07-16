6105bb997d0d77aeb7836cf25fe8c34636573613
sudobooo <marsdoghelena@gmail.com>
2022-07-17 00:58:43 +0300

    Fix urls

--- task_manager/users/urls.py
+++ task_manager/users/urls.py
@@ -2,14 +2,14 @@ from django.urls import path
 from task_manager.users.views import (
     ListOfUsers,
     CreateUser,
-    UpdateUser,
-    DeleteUser
+    # UpdateUser,
+    # DeleteUser
 )
 
 
 urlpatterns = [
     path('',  ListOfUsers.as_view(), name='list_of_users'),
     path('create/',  CreateUser.as_view(), name='create_user'),
-    path('<int:pk>/update/', UpdateUser.as_view(), name='update_user'),
-    path('<int:pk>/delete/', DeleteUser.as_view(), name='delete_user'),
+    # path('<int:pk>/update/', UpdateUser.as_view(), name='update_user'),
+    # path('<int:pk>/delete/', DeleteUser.as_view(), name='delete_user'),
 ]
