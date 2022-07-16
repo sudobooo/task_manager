b201b577d2b83915f79599204688d29c8b759e05
sudobooo <marsdoghelena@gmail.com>
2022-07-16 23:58:25 +0300

    Update settings

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -31,7 +31,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.getenv("DEBUG")
+DEBUG = os.getenv('DEBUG', 'true').lower() in {'yes', '1', 'true'}
 
 ALLOWED_HOSTS = [
     'vibrant-madame-12861.herokuapp.com',
