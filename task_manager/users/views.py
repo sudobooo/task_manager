e996bb53c43799fc3be19ad53f2e329249bf4481
sudobooo <marsdoghelena@gmail.com>
2022-07-18 13:41:22 +0300

    Update views

--- task_manager/users/views.py
+++ task_manager/users/views.py
@@ -1,15 +1,13 @@
 from task_manager.users.models import ApplicationUsers
 from task_manager.users.forms import SignInForm, SignUpForm
-from task_manager.mixin import HandleNoPermissionMixin
 
 from django.contrib import messages
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from django.contrib.auth.views import LoginView, LogoutView
 from django.contrib.messages.views import SuccessMessageMixin
-from django.db.models import ProtectedError
 from django.utils.translation import gettext_lazy, gettext
-from django.http import HttpResponseRedirect
 from django.urls import reverse_lazy
+from django.shortcuts import redirect
 from django.views.generic.list import ListView
 from django.views.generic.edit import (
     CreateView,
@@ -28,17 +26,15 @@ class ListOfUsers(ListView):
 
 class SignUp(CreateView, SuccessMessageMixin):
 
-    model = ApplicationUsers
     template_name = 'users/sign_up.html'
     form_class = SignUpForm
     success_url = reverse_lazy('sign_in')
-    success_message = gettext_lazy('Пользователь успешно зарегистрирован')
+    success_message = gettext('Пользователь успешно зарегистрирован')
 
 
 class UpdateUser(LoginRequiredMixin,
                  SuccessMessageMixin,
                  UserPassesTestMixin,
-                 HandleNoPermissionMixin,
                  UpdateView,
                  FormView, ):
 
@@ -47,40 +43,33 @@ class UpdateUser(LoginRequiredMixin,
     form_class = SignUpForm
     success_url = reverse_lazy('list_of_users')
     success_message = gettext_lazy('Пользователь изменён')
-    error_message = gettext_lazy('У вас нет прав для изменения другого\
-                                  пользователя.')
-    no_permission_url = 'list_of_users'
 
     def test_func(self):
         return self.request.user == self.get_object()
 
+    def handle_no_permission(self):
+        messages.error(self.request, gettext('У вас нет прав для изменения\
+                                             другого пользователя.'))
+        return redirect('list_of_users')
+
 
 class DeleteUser(LoginRequiredMixin,
                  SuccessMessageMixin,
-                 HandleNoPermissionMixin,
                  UserPassesTestMixin,
-                 DeleteView,
-                 FormView, ):
+                 DeleteView):
 
     model = ApplicationUsers
     template_name = 'users/delete_user.html'
     success_url = reverse_lazy('list_of_users')
     success_message = gettext_lazy('Пользователь успешно удалён')
-    error_message = gettext_lazy('У вас нет прав для изменения другого\
-                                  пользователя.')
-    no_permission_url = 'list_of_users'
-
-    def form_valid(self, form):
-        try:
-            self.object.delete()
-        except ProtectedError:
-            messages.error(self.request, gettext_lazy(self.error_message))
-        else:
-            messages.success(self.request, self.success_message)
-        return HttpResponseRedirect(self.success_url)
 
     def test_func(self):
         return self.request.user == self.get_object()
+
+    def handle_no_permission(self):
+        messages.error(self.request, gettext('У вас нет прав для изменения\
+                                             другого пользователя.'))
+        return redirect('list_of_users')
 
 
 class SignIn(SuccessMessageMixin, LoginView):
