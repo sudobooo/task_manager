d3565126a9430ae976df5e0e4043e4c0419cf9d9
sudobooo <marsdoghelena@gmail.com>
2022-07-21 17:34:03 +0300

    Add custom mixins

---
+++ task_manager/mixins.py
@@ -0,0 +1,44 @@
+from django.contrib import messages
+from django.contrib.auth.mixins import AccessMixin
+from django.shortcuts import redirect
+from django.urls import reverse_lazy
+from django.db.models import ProtectedError
+from django.http import HttpResponseRedirect
+from django.utils.translation import gettext
+
+
+class CheckSignInMixin(AccessMixin):
+    redirect_sign_in_name = 'sign_in'
+    error_sign_in_message = ''
+    request = ''
+
+    def handle_no_permission(self):
+        messages.error(self.request, gettext(self.error_sign_in_message))
+        return redirect(reverse_lazy(self.redirect_sign_in_name))
+
+
+class CheckDeleteMixin(AccessMixin):
+    error_delete_message = ''
+    success_delete_message = ''
+    success_delete_url = ''
+
+    def form_valid(self, form):
+        try:
+            self.object.delete()
+        except ProtectedError:
+            messages.error(self.request, gettext(self.error_delete_message))
+        else:
+            messages.success(self.request,
+                             gettext(self.success_delete_message))
+        return HttpResponseRedirect(reverse_lazy(self.success_delete_url))
+
+
+class CheckUpdateMixin(AccessMixin):
+    redirect_error_update_name = ''
+    error_update_message = ''
+
+    def dispatch(self, request, *args, **kwargs):
+        if self.request.user != self.get_object():
+            messages.error(self.request, gettext(self.error_update_message))
+            return redirect(reverse_lazy(self.redirect_error_update_name))
+        return super().dispatch(request, *args, **kwargs)
