09c09a9dc7773a2665a0f7b8a69805726c88b2cb
sudobooo <marsdoghelena@gmail.com>
2022-07-24 21:34:43 +0300

    Fix settings

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -76,23 +76,23 @@ WSGI_APPLICATION = 'task_manager.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.getenv('POSTGRES_NAME'),
-        'USER': os.getenv('POSTGRES_USER'),
-        'PASSWORD': os.getenv('POSTGRES_PASSWORD'),
-        'HOST': os.getenv('POSTGRES_HOST'),
-        'PORT': os.getenv('POSTGRES_PORT'),
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
 
-SQLITE_DATABASES = {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+POSTGRES_DATABASES = {
+    'ENGINE': 'django.db.backends.postgresql',
+    'NAME': os.getenv('POSTGRES_NAME'),
+    'USER': os.getenv('POSTGRES_USER'),
+    'PASSWORD': os.getenv('POSTGRES_PASSWORD'),
+    'HOST': os.getenv('POSTGRES_HOST'),
+    'PORT': os.getenv('POSTGRES_PORT'),
+}
 
 if os.getenv('DB_ENGINE') in ['True']:
-    DATABASES['default'] = SQLITE_DATABASES
+    DATABASES['default'] = POSTGRES_DATABASES
 
 AUTH_PASSWORD_VALIDATORS = [
     {
