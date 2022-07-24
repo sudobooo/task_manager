46542eb674afeb779abd8a827e454d8aa57d9eb4
sudobooo <marsdoghelena@gmail.com>
2022-07-24 21:04:20 +0300

    Add postgresql

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -76,10 +76,20 @@ WSGI_APPLICATION = 'task_manager.wsgi.application'
 
 DATABASES = {
     'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': os.getenv('POSTGRES_NAME'),
+        'USER': os.getenv('POSTGRES_USER'),
+        'PASSWORD': os.getenv('POSTGRES_PASSWORD'),
+        'HOST': os.getenv('POSTGRES_HOST'),
+        'PORT': os.getenv('POSTGRES_PORT'),
+    }
+}
+
+
+SQLITE_DATABASES = {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
-}
 
 
 AUTH_PASSWORD_VALIDATORS = [
