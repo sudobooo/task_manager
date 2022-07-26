a8816a84d1353f324c55369b50194225046bac89
sudobooo <sergey.masyutin@insales.ru>
2022-07-26 16:20:13 +0300

    Add main page

--- task_manager/urls.py
+++ task_manager/urls.py
@@ -4,7 +4,7 @@ from django.views.generic import TemplateView
 from task_manager.users.views import SignIn, SignOut
 
 urlpatterns = [
-    path('', TemplateView.as_view(template_name='index.html'), name='main'),
+    path('', TemplateView.as_view(template_name='main.html'), name='main'),
     path('users/', include('task_manager.users.urls'), name='users'),
     path('statuses/', include('task_manager.statuses.urls'), name='statuses'),
     path('labels/', include('task_manager.labels.urls'), name='labels'),
